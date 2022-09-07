0e2a5632ff86e123c2df40e6f53b54c7c5aa04c9
SireDCA <86893294+SireDCA@users.noreply.github.com>
2022-09-07 09:33:35 +0100

    Added Dark Theme Added responsiveness

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ function changeText() {
    quoteBtn.innerHTML = "Quote Sent";
    console.log(quoteBtn.innerHTML);
 }
+const btn = document.querySelector(".dark-mode-toggle");
+document.querySelector("html");
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+    //   document.body.classList.add("dark-theme");
+    document.querySelector("html").classList.add("dark");
+
+}
+btn.addEventListener("click", function () {
+    document.querySelector("body").classList.toggle("dark");
+    document.querySelector("header").classList.toggle("dark");
+    document.querySelector("#page-2").classList.toggle("dark");
+    document.querySelector(".middle-section").classList.toggle("dark");
+    document.querySelector("aside").classList.toggle("dark");
+    document.querySelector("footer").classList.toggle("dark");
+    document.querySelector(".dark-mode-toggle").classList.toggle("dark");
+
+
+    let theme = "light";
+    //   if (document.body.classList.contains("dark-theme")) {
+    if (document.querySelector("body").classList.contains("dark")) {
+
+        theme = "dark";
+    }
+    localStorage.setItem("theme", theme);
+});
