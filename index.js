ae686119a50392a4465867753e93d726930907b4
SireDCA <86893294+SireDCA@users.noreply.github.com>
2022-09-07 12:14:37 +0100

    Final commit

--- index.js
+++ index.js
@@ -4,17 +4,8 @@ let icons = document.querySelector("#ham");
 let hugs = document.querySelector("#hug");
 
 //Functions
-icons.onclick = function(){
-    showIcons();
-}
-
 quoteBtn.onclick = function() {
     changeText();
-}
-
-function showIcons() {
-    icons.style.display = "none"
-    hugs.style.display = "flex"
 }
 function changeText() {
    quoteBtn.innerHTML = "Sent";
